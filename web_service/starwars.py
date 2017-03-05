5d21e2344b4b3ba1b4d7f8986d2093b26d16193a
Rafael Ugolini <rafael.ugolini@gmail.com>
2017-03-05 17:13:26 +0100

    Add 20% chances of failure

--- web_service/starwars.py
+++ web_service/starwars.py
@@ -3,30 +3,41 @@ from random import randint
 import requests
 
 MIN_PERSON_ID = 1
-MAX_PERSON_ID = 88
+MAX_PERSON_ID = 104
 GATEWAY_DB = 'http://gateway:4984/db/'
 
 
 class Person:
 
     def get_random(self):
-        r = requests.get('http://swapi.co/api/people/{}'.format(
+        res = requests.get('http://swapi.co/api/people/{}'.format(
             randint(MIN_PERSON_ID, MAX_PERSON_ID)
         ))
-        self.person = r.json()
+        self.status_code = res.status_code
+        self.json = res.json()
 
     def save(self):
         self.get_random()
-        requests.put(
-            f'{GATEWAY_DB}{self.document_id}?rev={self.revision}',
-            json={
-                'name': self.person.get('name'),
-                'height': self.person.get('height'),
-                'mass': self.person.get('mass'),
-                'gender': self.person.get('gender'),
-                'action': 'person_success',
-            }
-        )
+        url = f'{GATEWAY_DB}{self.document_id}?rev={self.revision}'
+        if self.status_code == 200:
+            requests.put(
+                url,
+                json={
+                    'name': self.json.get('name'),
+                    'height': self.json.get('height'),
+                    'mass': self.json.get('mass'),
+                    'gender': self.json.get('gender'),
+                    'action': 'person_success',
+                }
+            )
+        else:
+            requests.put(
+                url,
+                json={
+                    'action': 'person_failure',
+                    'error': self.json
+                }
+            )
 
     def set_document_info(self, payload):
         self.document = payload
